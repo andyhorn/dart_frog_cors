17b61441ee1de825bb2fd8e84f531ec1e14ca96c
Andy Horn <andyjhorn@gmail.com>
2023-01-14 13:37:00 -0600

    add: OPTIONS to default methods

--- lib/src/cors_defaults.dart
+++ lib/src/cors_defaults.dart
@@ -2,7 +2,7 @@ class CorsDefaults {
   const CorsDefaults._();
 
   static const allowOrigin = '*';
-  static const allowMethods = 'GET,PUT,POST,PATCH,DELETE';
+  static const allowMethods = 'GET,PUT,POST,PATCH,DELETE,OPTIONS';
   static const allowHeaders =
       'Origin,X-Requested-With,Content-Type,Accept,Authorization';
 }
